cb1f6b2e8698b209165f685c0a94a63b25c8062c
hamza7681 <hamzambf@gmail.com>
2024-03-04 21:21:43 +0500

    env update

---
+++ envFile.js
@@ -0,0 +1,6 @@
+const envFile = {
+  PORT: 5000,
+  DB_URL:
+    "mongodb+srv://hamza7681:hamzadsc7681@cluster0.ttxcq.mongodb.net/desol-int?retryWrites=true&w=majority",
+  SECRET: "kdfhskjdfgsajvdbfhsmafdbfbjb",
+};
